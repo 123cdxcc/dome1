dfdadf804b2c268aece5ac2bc653ff79c7bf13e5
cdx <2409880020@qq.com>
2020-11-09 20:47:14 +0800

    完善添加fragment，home待完善

--- app/src/main/java/com/hnvist/apptest1/ui/HomeActivity.java
+++ app/src/main/java/com/hnvist/apptest1/ui/HomeActivity.java
@@ -1,16 +1,86 @@
 package com.hnvist.apptest1.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 import com.hnvist.apptest1.R;
+import com.hnvist.apptest1.ui.fragment.HjzbFragment;
+import com.hnvist.apptest1.ui.fragment.LssjFragment;
+import com.hnvist.apptest1.ui.fragment.SdkzFragment;
+import com.hnvist.apptest1.ui.fragment.XtkzFragment;
 
-public class HomeActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeActivity extends FragmentActivity implements View.OnClickListener {
+    private List<Fragment> mList;
+    private int fragmenIndex;
+    private FragmentManager manager;
+    private FragmentTransaction transaction;
+
+    private Fragment fragmentHjzb, fragmentLssj,fragmentSdkz,fragmentXtsz;
+    private FrameLayout fragmentMain;
+    private Button hjzb, lssj, sdkz, xtsz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        initView();
+    }
+
+    private void initView() {
+        fragmentMain = findViewById(R.id.fragmen_mian);
+
+        hjzb = findViewById(R.id.home_hjzb);
+        lssj = findViewById(R.id.home_lssj);
+        sdkz = findViewById(R.id.home_sdkz);
+        xtsz = findViewById(R.id.home_xtsz);
+
+
+        fragmentLssj = new LssjFragment();
+        fragmentSdkz = new SdkzFragment();
+        fragmentXtsz = new XtkzFragment();
+
+        mList = new ArrayList<>();
+        fragmenIndex = 0;
+        manager = getSupportFragmentManager();
+        transaction = manager.beginTransaction();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.home_hjzb:
+                if (fragmentHjzb == null){
+                    fragmentHjzb = new HjzbFragment();
+                    transaction.add(R.id.fragmen_mian, fragmentHjzb);
+                } else {
+                    transaction.show(fragmentHjzb);
+                }
+                break;
+            case R.id.home_lssj:
+
+                break;
+            case R.id.home_sdkz:
+
+                break;
+            case R.id.home_xtsz:
+
+                break;
+        }
+    }
+    private void fragmentShowOrHide(int index){
+
     }
 }
