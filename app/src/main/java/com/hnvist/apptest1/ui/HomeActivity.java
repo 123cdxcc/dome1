8ad50d2a438ff87f5ac93c7002c80b03c621a8bc
cdx <2409880020@qq.com>
2020-11-09 19:17:10 +0800

    添加主页activity，功能未完成

---
+++ app/src/main/java/com/hnvist/apptest1/ui/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.hnvist.apptest1.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hnvist.apptest1.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
